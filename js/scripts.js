25ee879acd95d75eb590975023f4455f7aff4b54
Ryan Duff <ryanduff613@gmail.com>
2020-03-30 15:18:47 -0700

    add submit but, add variable array1 to hold the value of userSentence.split(" ");

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     var userSentence = $('#userEntry').val();
     console.log(userSentence);
     
-    
-    
+    var array1 = userSentence.split(" ");
+    console.log(array1);
     
     
     event.preventDefault();
